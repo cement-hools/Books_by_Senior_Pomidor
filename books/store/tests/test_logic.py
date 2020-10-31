07425f09aff4f52c57e31ef78a1459914ecb6d8e
Максим Секачёв <cement-fan@ya.ru>
2020-10-31 19:13:02 +0300

    add test_logic

---
+++ books/store/tests/test_logic.py
@@ -0,0 +1,20 @@
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "books.settings")
+
+from django.test import TestCase
+
+from store.logic import operations
+
+
+class LogicTestCase(TestCase):
+    def test_plus(self):
+        result = operations(6, 13, '+')
+        self.assertEqual(19, result)
+
+    def test_minus(self):
+        result = operations(6, 13, '-')
+        self.assertEqual(-7, result)
+
+    def test_multyply(self):
+        result = operations(6, 13, '*')
+        self.assertEqual(78, result)
