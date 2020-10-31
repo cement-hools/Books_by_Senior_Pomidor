daa452b76cb0198f79faea4634e16b601766775d
Максим Секачёв <cement-fan@ya.ru>
2020-10-31 19:28:45 +0300

    add test_api

---
+++ books/store/tests/test_api.py
@@ -0,0 +1,18 @@
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from store.models import Book
+from store.serializers import BooksSerializer
+
+
+class BooksApiTestCase(APITestCase):
+    def test_get(self):
+        book_1 = Book.objects.create(name='test book 1', price=25)
+        book_2 = Book.objects.create(name='test book 2', price=55)
+        url = reverse('book-list')
+        response = self.client.get(url)
+        serializer_data = BooksSerializer([book_1, book_2], many=True).data # передаем список элементов и каждый серриализоввываем
+
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(serializer_data, response.data)
