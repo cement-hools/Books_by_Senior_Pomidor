29c734508afe8c1e20f942ab1d9f59fb155cf623
Максим Секачёв <cement-fan@ya.ru>
2020-10-31 19:40:49 +0300

    add test_serializers.py

--- books/store/views.py
+++ books/store/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 
 from store.models import Book
-from store.serializers import BooksSerializer
+from store.serializers import BookSerializer
 
 
 class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
-    serializer_class = BooksSerializer
+    serializer_class = BookSerializer
