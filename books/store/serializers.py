2dd996b8a4f7048e20dc6440b15a11f0f132e080
Максим Секачёв <cement-fan@ya.ru>
2020-11-02 08:34:29 +0300

    del get_likes_count

--- books/store/serializers.py
+++ books/store/serializers.py
@@ -5,20 +5,19 @@ from store.models import Book, UserBookRelation
 
 
 class BookSerializer(ModelSerializer):
-    likes_count = serializers.SerializerMethodField()
+    # likes_count = serializers.SerializerMethodField()
     annotated_likes = serializers.IntegerField(read_only=True)
     rating = serializers.DecimalField(max_digits=3, decimal_places=2,
                                       read_only=True)
 
     class Meta:
         model = Book
-        fields = ('id', 'name', 'price', 'author_name', 'likes_count',
+        fields = ('id', 'name', 'price', 'author_name',
                   'annotated_likes', 'rating',)
 
-    def get_likes_count(self,
-                        instance):  # instance - книга которую мы сериализуем
-        return UserBookRelation.objects.filter(book=instance,
-                                               like=True).count()
+    # def get_likes_count(self, instance):  # instance - книга которую мы сериализуем
+    #     return UserBookRelation.objects.filter(book=instance,
+    #                                            like=True).count()
 
 
 class UserBookRelationSerializer(ModelSerializer):
